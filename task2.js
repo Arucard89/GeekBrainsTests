51ff126ee1b1f58d6bb410370fe42dbfd031f9eb
Kirill <revenkov-k@mail.ru>
2018-06-01 16:28:57 +0300

    task 2 ready

--- task2.js
+++ task2.js
@@ -1,3 +1,8 @@
 /* Task 2 */
 
 /* Put your code here */
+var str = process.argv[1];
+var k = + process.argv[2];
+console.log(str.replace(/[a-z]/gi, function (c, offset, s) {
+    return String.fromCharCode(s.charCodeAt(offset) + k);
+}));
